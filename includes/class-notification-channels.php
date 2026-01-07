4293311ff3ea9678cca031971453be176cced8b8
Developer <dev@example.com>
2026-01-07 08:48:32 +0100

    fix: Use correct Slack markdown format
    - Changed headers from **text** to *text* for bold (Slack uses single asterisks)
    - Changed list items from * to - for proper bullet lists in Slack
    - Slack mrkdwn format uses - for lists, not *

--- includes/class-notification-channels.php
+++ includes/class-notification-channels.php
@@ -516,7 +516,7 @@ class PRM_Slack_Channel extends PRM_Notification_Channel {
         $text_parts = [];
         
         // Today section
-        $text_parts[] = '**' . __('Today', 'personal-crm') . '**';
+        $text_parts[] = '*' . __('Today', 'personal-crm') . '*';
         $text_parts[] = ''; // Empty line
         
         if (!empty($digest_data['today'])) {
@@ -531,20 +531,20 @@ class PRM_Slack_Channel extends PRM_Notification_Channel {
                 if ($person_in_title) {
                     // Replace name in title with link
                     $title_with_link = $this->replace_name_in_title_slack($date['title'], $person_in_title, $site_url);
-                    $text_parts[] = sprintf("* %s - %s", $title_with_link, $date_formatted);
+                    $text_parts[] = sprintf("- %s - %s", $title_with_link, $date_formatted);
                 } else {
                     // Show title normally
-                    $text_parts[] = sprintf("* %s - %s", $date['title'], $date_formatted);
+                    $text_parts[] = sprintf("- %s - %s", $date['title'], $date_formatted);
                 }
             }
         } else {
-            $text_parts[] = "* " . __('No reminders', 'personal-crm');
+            $text_parts[] = "- " . __('No reminders', 'personal-crm');
         }
         
         $text_parts[] = ''; // Empty line
         
         // Tomorrow section
-        $text_parts[] = '**' . __('Tomorrow', 'personal-crm') . '**';
+        $text_parts[] = '*' . __('Tomorrow', 'personal-crm') . '*';
         $text_parts[] = ''; // Empty line
         
         if (!empty($digest_data['tomorrow'])) {
@@ -557,19 +557,19 @@ class PRM_Slack_Channel extends PRM_Notification_Channel {
                 $person_in_title = $this->find_person_in_title($date['title'], $date['related_people']);
                 if ($person_in_title) {
                     $title_with_link = $this->replace_name_in_title_slack($date['title'], $person_in_title, $site_url);
-                    $text_parts[] = sprintf("* %s - %s", $title_with_link, $date_formatted);
+                    $text_parts[] = sprintf("- %s - %s", $title_with_link, $date_formatted);
                 } else {
-                    $text_parts[] = sprintf("* %s - %s", $date['title'], $date_formatted);
+                    $text_parts[] = sprintf("- %s - %s", $date['title'], $date_formatted);
                 }
             }
         } else {
-            $text_parts[] = "* " . __('No reminders', 'personal-crm');
+            $text_parts[] = "- " . __('No reminders', 'personal-crm');
         }
         
         $text_parts[] = ''; // Empty line
         
         // Rest of week section
-        $text_parts[] = '**' . __('Rest of the week', 'personal-crm') . '**';
+        $text_parts[] = '*' . __('Rest of the week', 'personal-crm') . '*';
         $text_parts[] = ''; // Empty line
         
         if (!empty($digest_data['rest_of_week'])) {
@@ -582,13 +582,13 @@ class PRM_Slack_Channel extends PRM_Notification_Channel {
                 $person_in_title = $this->find_person_in_title($date['title'], $date['related_people']);
                 if ($person_in_title) {
                     $title_with_link = $this->replace_name_in_title_slack($date['title'], $person_in_title, $site_url);
-                    $text_parts[] = sprintf("* %s - %s", $title_with_link, $date_formatted);
+                    $text_parts[] = sprintf("- %s - %s", $title_with_link, $date_formatted);
                 } else {
-                    $text_parts[] = sprintf("* %s - %s", $date['title'], $date_formatted);
+                    $text_parts[] = sprintf("- %s - %s", $date['title'], $date_formatted);
                 }
             }
         } else {
-            $text_parts[] = "* " . __('No reminders', 'personal-crm');
+            $text_parts[] = "- " . __('No reminders', 'personal-crm');
         }
         
         // Combine all text parts into a single block
