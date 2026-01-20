756c6ecc0631a5954f7fc29daf7a170e2204fe04
Developer <dev@example.com>
2026-01-20 21:44:31 +0100

    feat(92-01): expose list view properties in REST API
    - Add show_in_list_view and list_view_order to get_field_metadata() response
    - Add parameter definitions to get_create_params()
    - Add to optional_params in create_item() method
    - Add to updatable_params in update_item() method

--- includes/class-rest-custom-fields.php
+++ includes/class-rest-custom-fields.php
@@ -272,6 +272,14 @@ class CustomFields extends WP_REST_Controller {
 					$display_props['append'] = $field['append'];
 				}
 
+				// List view display settings.
+				if ( isset( $field['show_in_list_view'] ) ) {
+					$display_props['show_in_list_view'] = (bool) $field['show_in_list_view'];
+				}
+				if ( isset( $field['list_view_order'] ) ) {
+					$display_props['list_view_order'] = (int) $field['list_view_order'];
+				}
+
 				return $display_props;
 			},
 			$fields
@@ -314,6 +322,8 @@ class CustomFields extends WP_REST_Controller {
 			'post_type', 'filters',
 			// Color picker options.
 			'enable_opacity',
+			// List view display settings.
+			'show_in_list_view', 'list_view_order',
 		);
 		foreach ( $optional_params as $param ) {
 			if ( $request->has_param( $param ) ) {
@@ -383,6 +393,8 @@ class CustomFields extends WP_REST_Controller {
 			'post_type', 'filters',
 			// Color picker options.
 			'enable_opacity',
+			// List view display settings.
+			'show_in_list_view', 'list_view_order',
 		);
 		foreach ( $updatable_params as $param ) {
 			if ( $request->has_param( $param ) ) {
@@ -621,6 +633,17 @@ class CustomFields extends WP_REST_Controller {
 			'enable_opacity' => array(
 				'type'        => 'boolean',
 				'description' => 'Enable opacity/alpha slider (Color field)',
+			),
+			// List view display settings.
+			'show_in_list_view' => array(
+				'type'        => 'boolean',
+				'default'     => false,
+				'description' => 'Show field as column in list view',
+			),
+			'list_view_order' => array(
+				'type'        => 'integer',
+				'default'     => 999,
+				'description' => 'Column order in list view (lower = leftmost)',
 			),
 		);
 	}
